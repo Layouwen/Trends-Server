abef91b9fcfc5bb5ad50c13d26e64cc91171e870
layouwen <layouwen@gmail.com>
2020-06-13 18:42:02 +0800

    add register

--- server.js
+++ server.js
@@ -21,27 +21,55 @@ var server = http.createServer(function (request, response) {
 
     /******** main start ************/
 
-    response.statusCode = 200
-    let content
-    // setting index
-    const filePath = path === '/' ? '/index.html' : path
-    // judge type
-    const index = filePath.lastIndexOf('.')
-    const suffix = filePath.substring(index)
-    const fileType = {
-        '.html': 'text/html',
-        '.css': 'text/css',
-        '.js': 'text/javascript'
+    if (path === '/register' && method === 'POST') {
+        response.setHeader('Content-Type', 'text/html; charset=UTF-8')
+        let userArray = JSON.parse(fs.readFileSync('./database/users.json')) // read database
+        const array = []
+        request.on('data', (chunk) => {
+            array.push(chunk)
+
+        })
+        request.on('end', () => {
+            // convert string
+            const string = Buffer.concat(array).toString()
+            // convert obj
+            const obj = JSON.parse(string)
+            // last user id
+            const lastUser = userArray[userArray.length - 1]
+            // new user
+            const newUser = {
+                id: lastUser ? lastUser.id + 1 : 1,
+                name: obj.name,
+                password: obj.password
+            }
+            userArray.push(newUser)
+            // write data
+            fs.writeFileSync('./database/users.json', JSON.stringify(userArray))
+            response.end()
+        })
+    } else {
+        response.statusCode = 200
+        let content
+        // setting index
+        const filePath = path === '/' ? '/index.html' : path
+        // judge type
+        const index = filePath.lastIndexOf('.')
+        const suffix = filePath.substring(index)
+        const fileType = {
+            '.html': 'text/html',
+            '.css': 'text/css',
+            '.js': 'text/javascript'
+        }
+        response.setHeader('Content-Type', `${fileType[suffix] || "text/html"};charset=utf-8`)
+        try {
+            content = fs.readFileSync(`./public${filePath}`)
+        } catch (error) {
+            content = '文件路径不存在'
+            response.statusCode = 404
+        }
+        response.write(content)
+        response.end()
     }
-    response.setHeader('Content-Type', `${fileType[suffix] || "text/html"};charset=utf-8`)
-    try {
-        content = fs.readFileSync(`./public${filePath}`)
-    } catch (error) {
-        content = '文件路径不存在'
-        response.statusCode = 404
-    }
-    response.write(content)
-    response.end()
 
     /******** main end ************/
 })
